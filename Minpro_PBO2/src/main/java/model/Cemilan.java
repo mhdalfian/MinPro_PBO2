9fd941f3fe81cc2e89852f72b9752d8b0cf9d4f7
mhdalfian <144761136+mhdalfian@users.noreply.github.com>
2024-10-14 22:35:52 +0800

    Delete Minpro_PBO2 directory

--- Minpro_PBO2/src/main/java/model/Cemilan.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Cemilan extends Barang {
-    public Cemilan(String nama, double harga, int stok) {
-        super("Cemilan", nama, harga, stok);
-    }
-}
--- Minpro_PBO2/src/main/java/model/Minuman.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Minuman extends Barang {
-    public Minuman(String nama, double harga, int stok) {
-        super("Minuman", nama, harga, stok);
-    }
-}
